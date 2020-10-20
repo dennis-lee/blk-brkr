a00654e61ef5a866fbe59304f89e3f5da90474c5
Dennis Lee <dennis.lcfx@gmail.com>
2020-10-20 19:35:53 +0800

    Enable launching of Ball from Paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    Vector2 offset;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 ballPos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = ballPos + offset;
+    }
+}
