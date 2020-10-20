6c94ed898a23899353fc12e15af9a6007d0d069f
Dennis Lee <dennis.lcfx@gmail.com>
2020-10-20 18:52:33 +0800

    Enable Paddle to move horizontally

---
+++ Assets/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] static float paddleWidthInUnits = 2f;
+    private float stopLeft = paddleWidthInUnits / 2;
+    private float stopRight = 16f - (paddleWidthInUnits / 2);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalWidth = Camera.main.aspect * Camera.main.orthographicSize * 2;
+        float mouseX = Mathf.Clamp((Input.mousePosition.x / Screen.width * horizontalWidth), stopLeft, stopRight);
+        transform.position = new Vector2(mouseX, transform.position.y);
+    }
+}
