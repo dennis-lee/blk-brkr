c7cc8275a770b6c27b1cd98de65960034b528315
Dennis Lee <dennis.lcfx@gmail.com>
2020-10-21 19:59:15 +0800

    Play sound effects when Ball collides with other game objects

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,14 +8,17 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     Vector2 offset;
     bool hasStarted = false;
+    AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - paddle.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -41,5 +44,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 ballPos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = ballPos + offset;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }        
     }
 }
